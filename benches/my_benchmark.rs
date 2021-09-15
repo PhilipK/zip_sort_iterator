9bd324bdaa3699fed543f1b7697dc7198beb0f5d
Philip <philipkristoffersen@gmail.com>
2021-09-15 06:53:21 +0200

    Benchmark zip creation

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -17,7 +17,6 @@ fn zip_benchmark(cri: &mut Criterion) {
     }
 
     cri.bench_function("zip mixed", |b| {
-        
         b.iter(|| {
             let zip = PrioritySortIterator::new(vec![&va, &vb, &vc].as_mut_slice());
             black_box(zip.last());
@@ -25,5 +24,30 @@ fn zip_benchmark(cri: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, zip_benchmark);
+
+
+fn zip_creatrion(cri: &mut Criterion) {
+    let mut va = vec![];
+    let mut vb = vec![];
+    let mut vc = vec![];
+
+    for i in (0..10000000).step_by(2) {
+        va.push(i);
+    }
+    for i in (1000000..4000000).step_by(10) {
+        vb.push(i);
+    }
+    for i in 0..1000000 {
+        vc.push(i);
+    }
+
+    cri.bench_function("zip creation", |b| {
+        b.iter(|| {
+            let zip = PrioritySortIterator::new(vec![&va, &vb, &vc].as_mut_slice());
+            black_box(zip);
+        })
+    });
+}
+
+criterion_group!(benches, zip_benchmark,zip_creatrion);
 criterion_main!(benches);
